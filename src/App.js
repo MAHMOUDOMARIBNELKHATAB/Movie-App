2eee86b368c0924a5249f38c46d28105474868b6
MAHMOUDOMARIBNELKHATAB <momar351@yahoo.com>
2023-10-25 23:09:08 +0300

    Movie App project react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import NavBar from "./components/navbar/nav";
+import { BrowserRouter,Routes , Route } from "react-router-dom";
+import Home from "./components/Home/home";
+import LogIn from "./components/LogIn/login";
+import Movies from "./components/movies/Movies";
+import TVShows from "./components/TV Shows/TVShows";
+import Details from "./components/details/Details";
+import Signup from "./components/LogIn/Signup";
+
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Routes> 
+        <Route path="/"  element={<Home/>}/> 
+          <Route  path="/home/:mediatype/:id"  element={<Details/>}/>
+        <Route  path="/Movies"  element={<Movies/>}/> 
+        <Route  path="/TVShows"  element={<TVShows/>}/> 
+        <Route  path="/LogIn"  element={<LogIn/>}/> 
+        <Route  path="/LogIn/Signup"  element={<Signup/>}/> 
+
+      </Routes>
     </div>
+  </BrowserRouter>
   );
 }
 
